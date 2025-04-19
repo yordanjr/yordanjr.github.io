90df88fcc9a905f170121c44dea4a1b83beb2af1
yordanjr <reyes.yordan2020@gmail.com>
2025-04-19 12:46:19 -0400

    new astro site deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://astro-simple-portfolio.vercel.app/', // Required for sitemap -> Replace with your site's URL
+	site: 'https://yordanjr.github.io/', // Required for sitemap -> Replace with your site's URL
 	output: 'static',
 	integrations: [tailwind(), sitemap()],
 	adapter: vercel(),
